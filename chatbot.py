39dcf20614acce6d71d1b297a3dc07e843422f1d
Mini Agarwal <miniagarwal2002Gmail.com>
2025-10-24 14:19:46 +0530

    Added chat history to the model

--- chatbot.py
+++ chatbot.py
@@ -1,23 +1,26 @@
 from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
 import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="HuggingFaceTB/SmolLM3-3B",
+    repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
     task="text-generation"
 )
 
 model= ChatHuggingFace(llm =llm)
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful assistant")
+]
 
 while True:
     user = input("You:")
-    chat_history.append(user)
+    chat_history.append(HumanMessage(content = user))
     if user == "exit":
         break
-    res = model.invoke(user)
-    chat_history.append(res.content)
+    res = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=res.content))
     print(res.content)
